715f755a5613f3d9f61b08aa89a2a47b9394c168
tkudlinski <tomasz.kudlinski@gmail.com>
2024-01-02 17:21:16 +0100

    more styling

--- src/components/Item.rs
+++ src/components/Item.rs
@@ -3,9 +3,11 @@ use leptos::*;
 #[component]
 pub fn Item(value: String, img_src: String) -> impl IntoView {
     view! {
-        <li class="text-gray-800 mb-2">
-            <img src=img_src/>
-            <span>{value}</span>
+        <li class="bg-white rounded-md overflow-hidden shadow-md p-4">
+            <div class="flex items-center mb-3">
+                <img src=img_src class="w-12 h-12 rounded-full mr-2"/>
+                <span class="text-gray-800 font-semibold">{value}</span>
+            </div>
         </li>
     }
 }
